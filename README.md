10c0ed3b099803131f7a8fa423ef03471ab3bff7
Tom Yam <tomyam1@gmail.com>
2015-03-03 19:29:17 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # grunt-jasmine-npm
-Run jasmine tests with the [official](https://github.com/jasmine/jasmine-npm) runner
+Run jasmine tests with the [official](https://github.com/jasmine/jasmine-npm) runner.
+Uses [jasmine-reporters](https://github.com/larrymyers/jasmine-reporters) or [jasmine-spec-reporter](https://github.com/bcaudan/jasmine-spec-reporter) for reporting.
+
+
+## Example
+
+
+    jasmine: {
+      test: {
+        options: {
+          // defaults to [ 'JasmineSpecReporter' ]
+          reporters: [
+            'TerminalReporter'
+            {
+              type: 'JUnitXmlReporter',
+              options: {
+                savePath: 'reports'
+              }
+            }
+          ]
+        },
+        src: [
+
+          // helpers
+          'src/**/*.helper.js'
+          'src/**/*.helper.coffee'
+
+          // specs
+          'src/**/*.spec.js'
+          'src/**/*.spec.coffee'
+        ]
+      },
+    },
---
+++ tasks/jasmine.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var Jasmine = require('jasmine'),
+  JasmineReporters = require('jasmine-reporters'),
+  JasmineSpecReporter = require('jasmine-spec-reporter'),
+  Joi = require('joi');
+
+
+module.exports = function(grunt) {
+
+  grunt.registerMultiTask('jasmine', function() {
+
+    var done = this.async(),
+      options = this.options(),
+      jasmine = new Jasmine();
+
+    // JasmineSpecReporter - for jasmine-spec-reporter
+    // JUnitXmlReporter, NUnitXmlReporter, TapReporter, TeamCityReporter, TerminalReporter
+    var reporterNames = [ 'JasmineSpecReporter'].concat(Object.keys(JasmineReporters));
+
+    // validate options
+    var optionsValidationRet = Joi.validate(options, Joi.object().keys({
+      reporters: Joi.array().items(
+        Joi.alternatives().try(
+          Joi.valid(reporterNames)
+            .description('The type of reporter to use.'),
+          Joi.object().keys({
+            type: Joi.valid(reporterNames).required()
+              .description('The type of reporter to use.'),
+            options: Joi.object().unknown(true)
+              .description('The options to pass to the constructor of the reporter.')
+          })
+        )
+      ).min(1).default([ 'JasmineSpecReporter' ])
+        .description('A list of reporters to add to Jasmine')
+    }));
+
+    if (optionsValidationRet.err) {
+      return done(optionsValidationRet.err);
+    }
+
+    options = optionsValidationRet.value;
+
+    function getReporter(type, options) {
+      if (type == 'JasmineSpecReporter') {
+        return new JasmineSpecReporter(options);
+      }
+      return new JasmineReporters[type](options)
+    }
+
+    // add all reporters to jasmine
+    options.reporters.forEach(function(reporter) {
+      if (typeof reporter == 'string') {
+        jasmine.addReporter(getReporter(reporter));
+      } else {
+        jasmine.addReporter(getReporter(reporter.type, reporter.options));
+      }
+    });
+
+
+    jasmine.specFiles = this.filesSrc || [];
+
+    jasmine.configureDefaultReporter({
+      onComplete: function(passed) {
+        done(passed);
+      }
+    });
+
+    jasmine.execute();
+  });
+};
